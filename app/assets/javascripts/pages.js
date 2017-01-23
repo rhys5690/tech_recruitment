f44bc410d6d132db8dec29d477f40df12a82a08a
rhys5690 <rhys5690@gmail.com>
2017-01-23 15:48:23 +1100

    added javascript

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,1 +1,42 @@
-console.log("i work");
+
+
+var displayEnterName = function() {
+    var $enterName = $(".enter_name");
+    $enterName.removeClass("none");
+
+    // Grab the next button
+    var $enteredName = $(".entered_name");
+    $enteredName.on("click", function(e) {
+
+
+        //Prevent default page submit
+        e.preventDefault();
+
+
+        //remove class display none from the suburb div
+        $enterName.addClass("none");
+
+        //call function for display enter Front-end or Back-end
+        displayEnterFrontBack();
+
+    });
+};
+
+$(document).ready(function() {
+    console.log("hello");
+
+    var $findTechTalent = $(".find-tech-talent-button");
+
+    $findTechTalent.on("click", function() {
+
+        // Add css to form here
+
+        // end Add css to evaluation-container
+        $findTechTalent.addClass("none");
+        displayEnterName();
+
+
+
+
+    });
+});
